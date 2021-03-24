f11a6664b4cf37de92bcdf073ac55e3d41a85c56
Michalis Ioak <ioakimidismichalis@gmail.com>
2021-03-24 19:29:25 +0200

    first commit

---
+++ lib/src/api/add.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+import 'package:dart_ipfs_api/src/constants.dart';
+import 'package:http/http.dart' as http;
+
+class AddResponse {
+  final int bytes;
+  final String hash;
+  final String name;
+  final String size;
+
+  AddResponse(this.bytes, this.hash, this.name, this.size);
+}
+
+Future<AddResponse> add(
+  List<int> file, {
+  bool? quite,
+  bool? quiter,
+  bool? silent,
+  bool? progress,
+}) async {
+  var request = http.MultipartRequest("POST", Uri.parse(url + "add"));
+  request.files.add(http.MultipartFile.fromBytes('file', file));
+  request.fields['quite'] = quite.toString();
+  request.fields['quiter'] = quiter.toString();
+  //TODO
+  final response = await request.send();
+  if (response.statusCode == 201) {
+    final res = jsonDecode(await response.stream.bytesToString());
+    return AddResponse(res['Bytes'], res['Hash'], res['Name'], res['Size']);
+  } else {
+    throw Exception('Failed to add');
+  }
+}
